3e9e77d71bfafe95868ec8440626563310cfea17
burokrat12-code <burokrat12@gmail.com>
2026-02-13 01:15:47 +0300

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# Удаляем упоминание helloworld, если оно там было
+sed -i '/helloworld/d' feeds.conf.default
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# Добавляем только qfirehose (чистые исходники)
+echo 'src-git qfirehose https://github.com/miz-ar/openwrt-qfirehose.git' >> feeds.conf.default
