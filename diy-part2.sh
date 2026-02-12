e4bae612767f3b2aaec6009268fc85bcd90efb61
burokrat12-code <burokrat12@gmail.com>
2026-02-13 01:16:09 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
 
-# 1. Создаем папку в будущем образе OpenWrt
-# Папка files в корне сборки — это способ добавить свои файлы в прошивку
-mkdir -p files/usr/bin
-
-# 2. Скачиваем скомпилированный QFirehose специально для ARM64 (твой cAP ax)
-# Мы берем версию aarch64, так как armv7 у тебя не запустится
-wget -O files/usr/bin/qfirehose https://github.com/ich777/qfirehose/releases/latest/download/qfirehose-aarch64
-
-# 3. Даем файлу права на исполнение
-chmod +x files/usr/bin/qfirehose
