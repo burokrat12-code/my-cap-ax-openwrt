aeb6b85c0f72876952c06c0221df3027964a19b5
burokrat12-code <burokrat12@gmail.com>
2026-02-13 01:39:04 +0300

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-# Удаляем упоминание helloworld, если оно там было
-sed -i '/helloworld/d' feeds.conf.default
 
-# Добавляем только qfirehose (чистые исходники)
-echo 'src-git qfirehose https://github.com/miz-ar/openwrt-qfirehose.git' >> feeds.conf.default
