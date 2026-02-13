54ee9a14fdf45312c1e6a4ed04f7d56035e623f6
burokrat12-code <burokrat12@gmail.com>
2026-02-13 11:59:23 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-# Принудительно выставляем платформу MikroTik cAP ax
-echo 'CONFIG_TARGET_ipq60xx=y' > .config
-echo 'CONFIG_TARGET_ipq60xx_generic=y' >> .config
-echo 'CONFIG_TARGET_ipq60xx_generic_DEVICE_mikrotik_cap-ax=y' >> .config
 
-# Добавляем LuCI
-echo 'CONFIG_PACKAGE_luci=y' >> .config
-echo 'CONFIG_PACKAGE_luci-ssl=y' >> .config
